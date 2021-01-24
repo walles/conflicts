2000430aa48833a3a39eb49e757578d738685abb
Johan Walles <johan.walles@gmail.com>
2021-01-24 10:51:01 +0100

    Add stash conflict handling support

--- show-my-original-change.sh
+++ show-my-original-change.sh
@@ -3,13 +3,19 @@
 set -euo pipefail
 
 if [ -z "${1+x}" ] || [ ! -d "$1" ] ; then
-    echo "ERROR: Syntax: $0 <dir-with-conflict>"
-    echo
-    echo 'Directory dir-with-conflict will be examined for conflicts.'
+    >&2 echo "ERROR: Syntax: $0 <dir-with-conflict>"
+    >&2 echo
+    >&2 echo 'Directory dir-with-conflict will be examined for conflicts.'
     exit 1
 fi
 
 cd "$1"
+
+# See: https://github.com/desktop/desktop/issues/7831#issuecomment-503534419
+if ! git status --porcelain=2 | grep -Eq '^u ' ; then
+    >&2 echo "ERROR: No conflict detected, nothing to show"
+    exit 1
+fi
 
 if [ -f ".git/REBASE_HEAD" ] ; then
     git show "$(cat .git/REBASE_HEAD)"
@@ -21,9 +27,6 @@ if [ -f ".git/MERGE_HEAD" ] ; then
     exit
 fi
 
-# FIXME: Add support for stash-vs-commit conflicts
-
-echo "ERROR: No conflict found"
-echo
-echo "Either $1 contains no conflict, or the conflict type is not supported by this script."
-exit 1
+# We have a conflict, but it's neither a merge nor a rebase one, take our
+# chances on "git stash pop".
+git stash show -p
